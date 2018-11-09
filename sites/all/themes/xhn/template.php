9e26ef789c07b859985f6aed0f592f7e7f1a7ba6
Senya <suharchuk.dima@gmail.com>
2018-11-10 01:06:29 +0200

    Added child theme XHN

---
+++ sites/all/themes/xhn/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function xhn_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function xhn_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function xhn_preprocess_node(&$variables) {
+}
